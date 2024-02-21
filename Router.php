c0e000bd4281a3234e4e4f9eb8f02c788e10cbf5
elkiki99 <brossani23@gmail.com>
2024-02-21 16:03:54 -0300

    devwebcamp

--- Router.php
+++ Router.php
@@ -50,6 +50,7 @@ class Router
 
         // Utilizar el layout de acuerdo a la url
         $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        
         if(str_contains($url_actual, "/admin")) {
             include_once __DIR__ . '/views/admin-layout.php';
         } else {
